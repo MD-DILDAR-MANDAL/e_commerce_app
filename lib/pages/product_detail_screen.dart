fd377c58694f011bf1d2d067d77d5db78c125dfa
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-07-23 09:22:05 +0530

    feat: details from database

--- lib/pages/product_detail_screen.dart
+++ lib/pages/product_detail_screen.dart
@@ -1,116 +1,144 @@
 import 'package:e_commerce_app/global_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({required this.id, super.key});
-  final int id;
+  final id;
 
   @override
   State<ProductDetailScreen> createState() => _ProductDetailScreenState();
 }
 
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  Future<Map<String, dynamic>?> fetchProduct() {
+    final result = Supabase.instance.client
+        .from('products')
+        .select()
+        .eq('product_id', widget.id)
+        .maybeSingle();
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("details"),
+        actionsPadding: EdgeInsets.all(8.0),
+        title: Text(""),
         actions: [Icon(Icons.favorite_border, color: primary)],
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.network(
-                "https://images.unsplash.com/photo-1618354691373-d851c5c3a990?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                fit: BoxFit.fitWidth,
-                width: double.infinity,
-                height: 250,
-                errorBuilder: (context, error, stackTrace) =>
-                    Icon(Icons.broken_image),
-              ),
-            ),
+      body: FutureBuilder(
+        future: fetchProduct(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("Error:${snapshot.error}"));
+          }
+          if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('product not found'));
+          }
+          final product = snapshot.data!;
 
-            SizedBox(height: 20),
-
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "T-shirt",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-
-                  Text(
-                    "₹499",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: primary,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Description",
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-              ),
-            ),
-            Text(
-              '''
-Where does it come from?
-
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            ''',
-              style: TextStyle(color: const Color.fromARGB(255, 111, 111, 111)),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+          return SingleChildScrollView(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: primary),
-                  onPressed: () {
-                    print("pressed");
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(14, 8, 14, 8),
-                    child: Text(
-                      "Buy Now",
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    product['image_url'],
+                    fit: BoxFit.fitWidth,
+                    width: double.infinity,
+                    height: 250,
+                    errorBuilder: (context, error, stackTrace) =>
+                        Icon(Icons.broken_image),
                   ),
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    print("pressed");
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
-                    child: Icon(
-                      Icons.shopping_bag,
-                      color: Colors.grey,
-                      size: 30,
-                    ),
+
+                SizedBox(height: 20),
+
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        product['name'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+
+                      Text(
+                        "₹ ${product["price"]}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: primary,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Description",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                  ),
+                ),
+                Text(
+                  product['description'],
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 111, 111, 111),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: primary),
+                      onPressed: () {
+                        print("pressed");
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(14, 8, 14, 8),
+                        child: Text(
+                          "Buy Now",
+                          style: TextStyle(color: Colors.white, fontSize: 22),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(padding: EdgeInsets.zero),
+                      onPressed: () {
+                        print("pressed");
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
+                        child: Icon(
+                          Icons.shopping_bag,
+                          color: Colors.grey,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
