b514bd9b365f179e1b63830f95f0821f7ab3c84b
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-07-29 17:47:26 +0530

    feat: razorpay integrate

--- lib/pages/cart/checkout_screen.dart
+++ lib/pages/cart/checkout_screen.dart
@@ -1,9 +1,16 @@
+import 'package:e_commerce_app/service/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+
 import 'package:e_commerce_app/global_colors.dart';
 import 'package:e_commerce_app/modals/check_bill.dart';
 import 'package:e_commerce_app/routes/routes.dart';
-import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 class CheckoutScreen extends StatefulWidget {
   CheckoutScreen({required this.billData, super.key});
@@ -16,6 +23,7 @@ class CheckoutScreen extends StatefulWidget {
 class _CheckoutScreenState extends State<CheckoutScreen> {
   String address = "set address";
   bool isLoading = false;
+  late Razorpay _razorpay;
 
   Future<void> fetchGeolocation() async {
     setState(() {
@@ -81,8 +89,28 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    _razorpay.clear();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    final email = authService.user?.email;
     final data = widget.billData;
+
+    DateTime dateNow = DateTime.now().add(Duration(days: 7));
+    String formattedDate = DateFormat('EEE, d MMM').format(dateNow);
     return Stack(
       children: [
         Scaffold(
@@ -144,7 +172,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                     size: 46,
                     color: primary,
                   ),
-                  title: Text("date, time"),
+                  title: Text(formattedDate),
                 ),
                 Divider(),
                 ListTile(
@@ -182,7 +210,11 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                     leading: Icon(Icons.payment),
                     title: Text("Pay with Razorpay (UPI/Card/NetBanking)"),
                     onTap: () {
-                      //   _startRazorpayPayment(context, total);
+                      _startRazorpayPayment(
+                        context,
+                        data.total,
+                        email.toString(),
+                      );
                     },
                   ),
                 ),
@@ -197,5 +229,49 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
           ),
       ],
     );
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Payment Successful! Transaction ID: ${response.paymentId}',
+        ),
+      ),
+    );
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Payment Failed: ${response.message} \nDescription: ${response.message}',
+        ),
+      ),
+    );
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('External Wallet Selected: ${response.walletName}'),
+      ),
+    );
+  }
+
+  void _startRazorpayPayment(
+    BuildContext context,
+    double amount,
+    String email,
+  ) {
+    var options = {
+      'key': dotenv.env['razorpay_key_id'],
+      'amount': amount.toInt(),
+      'name': 'Ecommerce Store',
+      'description': 'Order Payment',
+      //'order_id': '',
+      'prefill': {'email': email},
+    };
+    _razorpay.open(options);
   }
 }
